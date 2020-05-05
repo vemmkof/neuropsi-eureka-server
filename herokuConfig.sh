cfffd38189e48e394a9cf81079488d993bf08a64
Victor Mujica <victor.mujica.aztech@gmail.com>
2020-05-05 12:48:49 -0500

    first commit

---
+++ herokuConfig.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+heroku config:set USERNAME=eureka-server
+heroku config:set PASSWORD=b9cd4bdd5f554c576ba9e85b097f2be0
+heroku config:set PROFILE=dev
+heroku config:set HOSTNAME=
+heroku config:set EUREKA_URL=<PASSWORD>
---
+++ src/test/java/com/ipn/escom/neuropsi/eureka/server/NeuropsiEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ipn.escom.neuropsi.eureka.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NeuropsiEurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
