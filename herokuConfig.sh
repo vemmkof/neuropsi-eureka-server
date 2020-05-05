927beafad77839e8fd57b0fd5962329fd820ccfd
Victor Mujica <victor.mujica.aztech@gmail.com>
2020-05-05 13:04:23 -0500

    heroku url

--- herokuConfig.sh
+++ herokuConfig.sh
@@ -3,4 +3,4 @@ heroku config:set USERNAME=eureka-server
 heroku config:set PASSWORD=b9cd4bdd5f554c576ba9e85b097f2be0
 heroku config:set PROFILE=dev
 heroku config:set HOSTNAME=safe-shore-20278.herokuapp.com
-heroku config:set EUREKA_URL=https://eureka-server:b9cd4bdd5f554c576ba9e85b097f2be0@safe-shore-20278.herokuapp.com/eureka/
+heroku config:set EUREKA_URL=https://eureka-server:b9cd4bdd5f554c576ba9e85b097f2be0@safe-shore-20278.herokuapp.com
