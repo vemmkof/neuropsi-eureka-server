bb2795bd423aec6f8d35fe41c56d9a6033e86b28
Victor Mujica <victor.mujica.aztech@gmail.com>
2020-06-09 14:50:49 -0500

    heroku config

--- herokuConfig.sh
+++ herokuConfig.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-heroku config:set USERNAME=eureka-server
-heroku config:set PASSWORD=b9cd4bdd5f554c576ba9e85b097f2be0
+heroku config:set USERNAME=neuropsi
+heroku config:set PASSWORD=0e4e034f3504195e386b1bdcf4fbfe78
 heroku config:set PROFILE=dev
 heroku config:set HOSTNAME=safe-shore-20278.herokuapp.com
-heroku config:set EUREKA_URL=https://eureka-server:b9cd4bdd5f554c576ba9e85b097f2be0@safe-shore-20278.herokuapp.com
+heroku config:set EUREKA_URL=https://neuropsi:0e4e034f3504195e386b1bdcf4fbfe78@safe-shore-20278.herokuapp.com
